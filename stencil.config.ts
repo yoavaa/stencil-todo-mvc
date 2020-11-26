a3863fad32833d893de7abe4a84810c9e04ef001
yoavaa <yoav@wix.com>
2020-11-26 10:07:13 +0200

    added the todomvc sidebar

--- stencil.config.ts
+++ stencil.config.ts
@@ -16,7 +16,13 @@ export const config: Config = {
     },
     {
       type: 'www',
-      serviceWorker: null // disable service workers
+      serviceWorker: null, // disable service workers
+      copy: [
+        {src: 'index.css'},
+        {src: 'learn.json'},
+        {src: '../node_modules/todomvc-common/base.css', dest: 'todomvc-common-base.css'}
+        {src: '../node_modules/todomvc-common/base.js', dest: 'todomvc-common-base.js'}
+      ]
     }
   ]
 };
